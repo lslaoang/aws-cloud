8797adc6fef747e974df45cfaf0ff5515a1140aa
Laurel Lao Ang <laurellaoang@gmail.com>
2022-12-26 22:51:14 +0800

    update error-404.html

--- src/main/java/com/aws/basic/controller/IndexController.java
+++ src/main/java/com/aws/basic/controller/IndexController.java
@@ -2,13 +2,11 @@ package com.aws.basic.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("")
 public class IndexController {
 
-    @GetMapping
+    @GetMapping({"/","", "/index", "index.html"})
     public String getIndex(){
         return "index";
 
